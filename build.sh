7ba679b7a2f3eecd1e66ed8c27124113158300d5
cai.rong <cai.rong@yottabyte.cn>
2017-03-01 15:52:25 +0800

    修正build.sh编译sofarpc时的错误

--- build.sh
+++ build.sh
@@ -11,25 +11,26 @@ rm -rf depends && mkdir -pv depends
 cd $base_dir/thirdparty
 wget -c https://github.com/google/snappy/releases/download/1.1.4/snappy-1.1.4.tar.gz
 tar zxf snappy-1.1.4.tar.gz && cd snappy-1.1.4 && autoreconf -fvi &&  ./configure --prefix=$base_dir/depends && make -j4 && make install
-rm -rf ../snappy-1.1.4
+rm -rf ../snappy-1.1.4 ../snappy-1.1.4.tar.gz
 
 cd $base_dir/thirdparty
 wget -c https://github.com/google/protobuf/releases/download/v2.6.1/protobuf-2.6.1.tar.gz
 tar zxf protobuf-2.6.1.tar.gz && cd protobuf-2.6.1 && ./configure --disable-shared --with-pic --prefix $base_dir/depends && make -j4 && make install
-rm -rf ../protobuf-2.6.1
+rm -rf ../protobuf-2.6.1 ../protobuf-2.6.1.tar.gz
 
 cd $base_dir/thirdparty
 wget -c https://github.com/gflags/gflags/archive/v2.2.0.tar.gz
 tar xf v2.2.0.tar.gz && cd gflags-2.2.0 && cmake -DGFLAGS_NAMESPACE=google -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_INSTALL_PREFIX:PATH="$base_dir/depends" && make -j4 && make install
-rm -rf ../gflags-2.2.0
+rm -rf ../gflags-2.2.0 ../v2.2.0.tar.gz
 
 cd $base_dir/thirdparty
 wget -c http://d3dr9sfxru4sde.cloudfront.net/boost_1_61_0.tar.gz
 tar xf boost_1_61_0.tar.gz -C ${base_dir}/depends
+rm -rf ../boost_1_61_0.tar.gz
 
 cd $base_dir/thirdparty
 git clone https://github.com/baidu/sofa-pbrpc.git sofa-pbrpc
-cd sofa-pbrpc && make -e BOOST_HEADER_DIR=/data/ins/depends/boost_1_61_0 PROTOBUF_DIR=/data/ins/depends SNAPPY_DIR=/data/ins/depends OPT="-O2 -Wno-unused-parameter" -j4 && make -e PREFIX=/data/ins/depends instal
+cd sofa-pbrpc && make -e BOOST_HEADER_DIR=${base_dir}/depends/boost_1_61_0 PROTOBUF_DIR=${base_dir}/depends SNAPPY_DIR=${base_dir}/depends OPT="-O2 -Wno-unused-parameter" -j4 && make -e PREFIX=${base_dir}/depends install
 rm -rf ../sofa-pbrpc
 
 cd $base_dir
